0ea75940719620bd0cfb9573eeb2619db369c85f
Preedarat Chuthong <preedarat@example.com>
2025-03-06 11:38:09 +0700

    adjust auto redirect

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,17 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_APP_ANDROID_PACKAGE: string;
+    NEXT_PUBLIC_APP_ANDROID_SCHEME: string;
+    NEXT_PUBLIC_APP_IOS_SCHEME: string;
+    NEXT_PUBLIC_APP_STORE_LINK: string;
+    NEXT_PUBLIC_PLAY_STORE_LINK: string;
+    NEXT_PUBLIC_FALLBACK_URL: string;
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,33 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || (window as unknown as { opera: string }).opera;
+    const fallbackUrl = process.env.NEXT_PUBLIC_FALLBACK_URL ?? "https://chargingc.com";
+    const androidScheme = process.env.NEXT_PUBLIC_APP_ANDROID_SCHEME;
+    const androidPackage = process.env.NEXT_PUBLIC_APP_ANDROID_PACKAGE;
+    const playStoreLink = process.env.NEXT_PUBLIC_PLAY_STORE_LINK ?? "https://play.google.com/store";
+    const iosScheme = process.env.NEXT_PUBLIC_APP_IOS_SCHEME;
+    const appStoreLink = process.env.NEXT_PUBLIC_APP_STORE_LINK ?? "https://apps.apple.com/";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    let appLink = fallbackUrl;
+
+    if (/android/i.test(userAgent)) {
+      appLink = `intent://home#Intent;scheme=${androidScheme};package=${androidPackage};end;`;
+      setTimeout(() => {
+        window.location.href = playStoreLink;
+      }, 3000);
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !('MSStream' in window)) {
+      appLink = `${iosScheme}://home`;
+      setTimeout(() => {
+        window.location.href = appStoreLink;
+      }, 3000);
+    }
+
+    window.location.href = appLink;
+  }, []);
+
+  return <p>กำลังเปลี่ยนเส้นทาง...</p>;
 }
